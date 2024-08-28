69d5ea6da787f98de1a75b507151281e2c688b83
Mustack-Rabbi <mustackrabbi@gmail.com>
2024-08-28 22:15:25 +0600

    first digine

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -15,13 +15,70 @@ class _HomePageState extends State<HomePage> {
         title: Text("BMI CALCULATOR"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Row(children: [
-            Container(),
-            Container()
-          ],)
-        ],
+      body: Container(
+        color: Colors.black12,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: Colors.amber,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("CALCULATE"),)
+            ],
+          ),
+        ),
       ),
     );
   }
