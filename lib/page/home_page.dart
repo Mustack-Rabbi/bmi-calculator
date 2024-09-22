46dfda45727c01f19497fb1ab8036537cd2440b8
Mustack-Rabbi <mustackrabbi@gmail.com>
2024-09-22 07:39:58 +0600

    Deploy Flutter Web to gh-pages

---
+++ lib/page/home_page.dart
@@ -0,0 +1,254 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _currentSliderValue = 20;
+  int weight = 0;
+  int age = 0;
+  String result = "";
+  double bmi = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.black12,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.male), Text("MALE")],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: (){},
+                      child: Container(
+                        height: 100,
+                        color: Colors.amber,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [Icon(Icons.female), Text("FEMALE")],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.amber,
+                  child: Column(
+                    children: [
+                      Text("HEIGHT",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _currentSliderValue.round().toString(),
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
+                          ),
+                          Text("cm")
+                        ],
+                      ),
+                      Slider(
+                        value: _currentSliderValue,
+                        max: 200,
+                        onChanged: (double value) {
+                          setState(() {
+                            _currentSliderValue = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                      child: Column(
+                        children: [
+                          Text("WEIGHT",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(
+                            weight.toString(),
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: const ShapeDecoration(
+                                  color: Colors.black,
+                                  shape: CircleBorder(),
+                                ),
+                                child: IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    if (weight > 0) {
+                                      weight--;
+                                    }
+
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                decoration: const ShapeDecoration(
+                                  color: Colors.black,
+                                  shape: CircleBorder(),
+                                ),
+                                child: IconButton(
+                                  icon: const Icon(Icons.add),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    weight++;
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: Colors.amber,
+                      child: Column(
+                        children: [
+                          Text("AGE",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(
+                            age.toString(),
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: const ShapeDecoration(
+                                  color: Colors.black,
+                                  shape: CircleBorder(),
+                                ),
+                                child: IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    if (age > 0) {
+                                      age--;
+                                    }
+
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                decoration: const ShapeDecoration(
+                                  color: Colors.black,
+                                  shape: CircleBorder(),
+                                ),
+                                child: IconButton(
+                                  icon: const Icon(Icons.add),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    age++;
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  bmi = weight / pow(_currentSliderValue / 100, 2);
+
+                  if (bmi > 25) {
+                    result = "Overweight";
+                  } else if (bmi > 18.5) {
+                    result = "Normal";
+                  } else {
+                    result = "Underweight";
+                  }
+
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text("You Are $result"),
+                      //  content: const Text("your hight , age"),
+                    ),
+                  );
+                  setState(() {});
+                },
+                child: Text("CALCULATE"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
